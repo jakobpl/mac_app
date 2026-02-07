3661d00ffc0e1ae8550e68004194a7ca986a3ca5
jakob <jakobnorman46@gmail.com>
2026-02-07 11:34:47 -0600

    initial commit mac app

---
+++ mac_app_testing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mac_app_testing
+//
+//  Created by jakob n on 2/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mac_app_testing/mac_app_testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac_app_testingApp.swift
+//  mac_app_testing
+//
+//  Created by jakob n on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct mac_app_testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
